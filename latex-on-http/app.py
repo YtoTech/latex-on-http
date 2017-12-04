3ad7a181c7172c7c9109f4feb802ce9afb17b056
Yoan Tournade <yoan@ytotech.com>
2017-12-04 17:43:37 +0100

    Use threaded mode

--- latex-on-http/app.py
+++ latex-on-http/app.py
@@ -113,4 +113,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Latex on HTTP.')
     parser.add_argument('--verbose', action='store_true', help='Print verbose logging to stdout', default=False)
     args = parser.parse_args()
-    app.run(host='0.0.0.0', port=8080, debug=args.verbose)
+    app.run(host='0.0.0.0', port=8080, debug=args.verbose, threaded=True)
