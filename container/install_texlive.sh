13d41ab98f148716881172e011f1e6cd408e4f3e
Yoan Tournade <yoan@ytotech.com>
2017-12-05 16:18:33 +0100

    Try to run script in .travis before_install

---
+++ container/install_texlive.sh
@@ -0,0 +1,22 @@
+apt-get update
+apt-get install -y \
+    # TODO How do we select the list of Latex packages to install?
+    # (texlive-full is heavy!)
+    biber \
+    wget \
+    xzdec \
+    fontconfig \
+    latex-xcolor \
+    preview-latex-style \
+    texlive-bibtex-extra \
+    texlive-fonts-extra \
+    texlive-generic-extra \
+    # Heavy one, but we got all languages.
+    texlive-lang-all \
+    texlive-latex-base \
+    texlive-latex-extra \
+    texlive-latex-recommended \
+    texlive-luatex \
+    texlive-math-extra \
+    texlive-xetex \
+    texlive-science
