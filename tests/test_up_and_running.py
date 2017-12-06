a0f9d5c0541e98a21701332214e5e4719affcc3e
Yoan Tournade <yoan@ytotech.com>
2017-12-06 05:29:25 +0100

    Compare generated PDF to a reference stored version

--- tests/test_up_and_running.py
+++ tests/test_up_and_running.py
@@ -9,6 +9,9 @@
 """
 import pytest
 import requests
+import os
+
+SAMPLE_DIR = os.getcwd() + '/tests/samples/'
 
 COMPIL_HELLO_WORLD = {
     'resources': [
@@ -17,7 +20,18 @@ COMPIL_HELLO_WORLD = {
         }
     ]
 }
-PDF_HELLO_WORLD = ''
+PDF_HELLO_WORLD = SAMPLE_DIR + 'hello_world.pdf'
+
+def compareToSample(r, samplePath):
+    with open(PDF_HELLO_WORLD, 'rb') as f:
+        sample = f.read()
+        assert len(r.content) == len(sample)
+        # Generated binary PDF files differs.
+        # TODO Use https://github.com/euske/pdfminer to compare?
+        # assert generated == sample
+        # Or read as string (utf-8) and compare N first charachers.
+        # We may also find at which offset(s) the content differ between
+        # compilation and compare all but that.
 
 def test_api_index_redirect(latex_on_http_api_url):
     r = requests.get(latex_on_http_api_url, allow_redirects=False)
@@ -28,11 +42,13 @@ def test_simple_compilation_body(latex_on_http_api_url):
     r = requests.post(
         latex_on_http_api_url + '/compilers/latex', json=COMPIL_HELLO_WORLD
     )
-    assert r.status_code == 200
+    assert r.status_code == 201
+    compareToSample(r, PDF_HELLO_WORLD)
 
 # TODO API ping
 
-# TODO We can compile a simple document /compilers/latex
+# with open('hello_world.pdf', 'wb') as f:
+#     f.write(r.content)
 
 # TODO We can compile concurrently
 # when one compilation is running the API still respond
