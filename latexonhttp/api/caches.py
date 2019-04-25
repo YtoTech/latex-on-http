08396b84d1f89b5206b0e7413b6afc525c5f7014
Yoan Tournade <yoan@ytotech.com>
2019-04-26 01:20:05 +0200

    Do not let the cache entries hashes be public

--- latexonhttp/api/caches.py
+++ latexonhttp/api/caches.py
@@ -18,15 +18,24 @@ logger = logging.getLogger(__name__)
 
 caches_app = Blueprint("caches", __name__)
 
-# TODO Make this route private to the admin:
-# this allows to get all cache file hashes
-# and to actually get the file content,
-# by dumping them in Latex output.
-# Only provide the checker endpoint to the public.
+
 @caches_app.route("/resources", methods=["GET"])
 def resources_metadata():
-    return (jsonify(get_cache_metadata_snapshot()), 200)
+    return (jsonify(map_cache_metadata_for_public(get_cache_metadata_snapshot())), 200)
 
 
 # TODO Check for a list of resource hashes to know if there are cached.
 # POST:/caches/resources/check_cached
+
+def map_cache_metadata_for_public(cache_metadata):
+    # Filter out cache entries (or at least their hashes),
+    # because they would potentially allow user to get
+    # any cache file content (by dumping them in Latex output).
+    return {
+        **cache_metadata,
+        "cached_resources": [
+            {
+                "size": cached_resource["size"]
+            } for cached_resource in cache_metadata["cached_resources"].values()
+        ]
+    }
