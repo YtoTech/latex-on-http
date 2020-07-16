0ef4678852ffc3a6759271005ec3820a37cc3eb8
Yoan Tournade <y@mandelbro.com>
2020-07-16 15:52:27 +0200

    Update number of packages

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -26,7 +26,7 @@ def test_api_packages_list(latex_on_http_api_url):
         assert "url_ctan" in package
         assert "installed" in package
         assert package["installed"] is True
-    assert len(packages) == 4034
+    assert len(packages) == 4042
 
 
 PACKAGE_INFO_MANDATORIES_PROPERTIES = [
